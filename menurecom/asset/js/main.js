bf381fe329bc53b7c3f4ac2461d64adaa70bcaed
kittty0520 <105909450+kittty0520@users.noreply.github.com>
2022-12-29 16:25:32 +0900

    Remove Folders

--- menurecom/asset/js/main.js
+++ menurecom/asset/js/main.js
@@ -1,33 +1,71 @@
-/*메뉴를 필터링 */
-
-let foodArr = [
-    {
-        이름:'짜장면',
-        매움:'N',
-        종류:'한식',
-    },
-    {
-        이름:'탕수육',
-        매움:'Y',
-        종류:'양식',
-    }
-]
-
-function FilterName(name,arr){
-    return arr.filter((value)=>value.종류===name);
-}
-
-const result =FilterName('양식',foodArr);
-
-console.log(result);
-
-
 /*질문을 바꾸는 함수*/
 
-const questionList={
-    '첫번째 질문입니다.',
-    '두번째 질문입니다.',
-    '세번째 질문입니다.',
+const questionList = [
+    '당신의 직급은  무엇입니까?',
+    '오늘의 점심은 어느 나라 음식을 드시겠습니까?',
+    '어떤 식재료를 넣을까요?',
+]
+
+const  answerList = [
+     // 1 
+    ['신입','대리','과장','부장','대표'],
+    // 2
+    ['한식','양식','중식','일식','기타'],
+    // 3
+    ['쌀','밀가루','육류','해산물','채소'],
+]
+
+const startBtn = document.querySelector('.start__button');
+const startTitle = document.querySelector('.start__title');
+const startPage = document.querySelector('.start');
+
+const questions = document.querySelector('.questions');
+const question = document.querySelector('.question'); 
+const answer = document.querySelector('.answers__list');
+
+startBtn.addEventListener('click',()=>{
+    CloseStart();
+    questions.style.display='block';
+    StartQuestion();
+});
+
+function CloseStart(){
+    startPage.style.display='none';
 }
 
-const 
+function StartQuestion(){
+    question.innerHTML='';
+    addItem('newQuestion', question);
+    addItem('newAnswer', answer);
+}
+
+function addItem(className, field){
+    const item = document.createElement('div');
+    item.setAttribute('class', className);
+    item.style.border="1px solid #333";
+    item.innerHTML= field === question? questionSet(): answerSet();
+    field.appendChild(item);
+}
+
+function questionSet(){
+    let questionNum = 0;
+    const newQuestion = questionList[questionNum];
+    questionNum++;
+    return newQuestion;
+}
+
+function answerSet(){
+    let answerNum = 0;
+    let newAnswer = answerList[answerNum];
+    return newAnswer.map(item=>styleAnswer(item));
+}
+
+function styleAnswer(item){
+    const answer = document.createElement('button');
+    answer.setAttribute('class', 'answer__each');
+    answer.style.border = '1px solid #666';
+    answer.style.margin = '10px';
+    answer.innerHTML=item;
+}
+
+
